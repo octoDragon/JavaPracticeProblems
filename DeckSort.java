b99921f96222ea6a3f2d52b44c1ac98c7e9ae0de
Kristoff Finley <contentconsuming@gmail.com>
2020-09-02 23:45:19 -0600

    added the deck sort problem to the file

---
+++ DeckSort.java
@@ -0,0 +1,11 @@
+//Explain how you would put a deck of cards in order by suit(in the
+// order spades,hearts,clubs,diamonds)and by rank within each suit,with
+// the restriction that the cards must be laid out face down in a row,and
+// the only allowed operations are to check the values of two cards and to
+// exchange two cards(keeping them face down).
+
+public class DeckSort {
+    public static void main(String[] args) {
+
+    }
+}
